c899ceb75fea37c9818c147168afde094d5098e0
Denys Bushuliak <denys@bynyk.com>
2023-09-01 20:05:56 +0300

    feat: blocking

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,16 @@ impl<'client> Client<'client> {
         }
     }
 
+    #[cfg(feature = "blocking")]
+    pub fn ping(&self) -> Result<bool> {
+        let response = reqwest::blocking::get(format!("{}/ping", self.server_url))?;
+
+        let text = response.text()?;
+
+        Ok(text == "Ok.\n")
+    }
+
+    #[cfg(not(feature = "blocking"))]
     pub async fn ping(&self) -> Result<bool> {
         let response = reqwest::get(format!("{}/ping", self.server_url)).await?;
 
@@ -22,6 +32,22 @@ impl<'client> Client<'client> {
         Ok(text == "Ok.\n")
     }
 
+    #[cfg(feature = "blocking")]
+    pub fn execute(&self, query: impl Into<String>) -> Result<()> {
+        let response = reqwest::blocking::Client::new()
+            .post(self.server_url)
+            .body(query.into())
+            .send()?;
+
+        if !response.status().is_success() {
+            let body = response.text()?;
+            bail!("Error: {}", body);
+        }
+
+        Ok(())
+    }
+
+    #[cfg(not(feature = "blocking"))]
     pub async fn execute(&self, query: impl Into<String>) -> Result<()> {
         let response = reqwest::Client::new()
             .post(self.server_url)
@@ -36,6 +62,32 @@ impl<'client> Client<'client> {
         Ok(())
     }
 
+    #[cfg(feature = "blocking")]
+    pub fn query<TResult>(&self, query: impl Into<String>) -> Result<QueryResult<TResult>>
+    where
+        TResult: serde::de::DeserializeOwned,
+    {
+        let query = query.into();
+        if query.is_empty() {
+            bail!("Query is empty");
+        }
+        if !query.contains("FORMAT JSON") {
+            bail!("Query must contains FORMAT JSON");
+        }
+
+        let response = reqwest::blocking::Client::new()
+            .post(self.server_url)
+            .body(query)
+            .send()?;
+
+        if !response.status().is_success() {
+            bail!("Error: {}", response.text()?);
+        }
+
+        Ok(response.json::<QueryResult<TResult>>()?)
+    }
+
+    #[cfg(not(feature = "blocking"))]
     pub async fn query<TResult>(&self, query: impl Into<String>) -> Result<QueryResult<TResult>>
     where
         TResult: serde::de::DeserializeOwned,
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,20 @@
 use anyhow::Result;
 use clickhouse_rs::Client;
 
+#[cfg(not(feature = "blocking"))]
 async fn seed() -> Result<()> {
     let client = Client::new("http://localhost:8123");
+    client.execute("DROP TABLE test").await?;
     client.execute("CREATE TABLE IF NOT EXISTS test(name String, age Int32, PRIMARY KEY name) ENGINE = MergeTree;").await?;
+
+    Ok(())
+}
+
+#[cfg(feature = "blocking")]
+fn seed() -> Result<()> {
+    let client = Client::new("http://localhost:8123");
+    client.execute("DROP TABLE test");
+    client.execute("CREATE TABLE IF NOT EXISTS test(name String, age Int32, PRIMARY KEY name) ENGINE = MergeTree;")?;
 
     Ok(())
 }
@@ -17,7 +28,57 @@ mod tests {
 
     use crate::seed;
 
+    #[test]
+    #[cfg(feature = "blocking")]
+    fn test_blocking_ping() -> Result<()> {
+        let client = Client::new("http://localhost:8123");
+        let is_ok = client.ping()?;
+        assert!(is_ok);
+        Ok(())
+    }
+
+    #[test]
+    #[cfg(feature = "blocking")]
+    fn test_blocking_create_table() -> Result<()> {
+        let client = Client::new("http://localhost:8123");
+
+        client
+            .execute(
+                "CREATE TABLE IF NOT EXISTS test(name String, age Int32, PRIMARY KEY name) ENGINE = MergeTree;",
+            )?;
+
+        Ok(())
+    }
+
+    #[test]
+    #[cfg(feature = "blocking")]
+    fn test_blocking_insert() -> Result<()> {
+        seed()?;
+
+        #[derive(Debug, Deserialize)]
+        struct User {
+            pub name: String,
+            pub age: i32,
+        }
+        let client = Client::new("http://localhost:8123");
+
+        client.execute("INSERT INTO test(name, age) VALUES ('John', 42) ;")?;
+        client.execute("INSERT INTO test(name, age) VALUES ('Tommy', 34) ;")?;
+
+        let result = client.query::<User>("SELECT * from test ORDER BY name ASC FORMAT JSON")?;
+
+        assert_eq!(result.data.len(), 2);
+        assert_eq!(result.data[0].name, "John");
+        assert_eq!(result.data[0].age, 42);
+        assert_eq!(result.data[1].name, "Tommy");
+        assert_eq!(result.data[1].age, 34);
+        assert_eq!(result.statistics.rows_read, 2);
+
+        Ok(())
+    }
+
     #[tokio::test]
+    #[cfg(not(feature = "blocking"))]
     async fn test_ping() -> Result<()> {
         let client = Client::new("http://localhost:8123");
         let is_ok = client.ping().await?;
@@ -26,6 +87,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[cfg(not(feature = "blocking"))]
     async fn test_create_table() -> Result<()> {
         let client = Client::new("http://localhost:8123");
 
@@ -39,6 +101,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[cfg(not(feature = "blocking"))]
     async fn test_insert() -> Result<()> {
         seed().await?;
 
